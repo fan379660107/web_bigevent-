10306d2dc0b5577aa60fc6a1cfe81d8e624719cd
fan379660107 <379660107@qq.com>
2022-06-09 20:06:20 +0800

    今日完成退出功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,13 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((options) => {
-    console.log(options);
-    //在请求前拼接上根路径
-    options.url = 'http://www.liulongbin.top:3007' +options.url
+  console.log(options);
+  //在请求前拼接上根路径
+  options.url = 'http://www.liulongbin.top:3007' + options.url;
+  //注入 token
+  if (options.url.includes('/my/')) {
+    options.headers = {
+      Authorization: localStorage.getItem('token'),
+    };
+  }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+// 获取用户信息
+function getUserInfo() {
+  $.ajax({
+    type: 'GET',
+    url: '/my/userinfo',
+    // headers: {
+    //   Authorization: localStorage.getItem('token'),
+    // },
+    success: (res) => {
+      //   console.log(res);
+      if (res.status !== 0) return layer.msg('获取用户信息失败！');
+      layer.msg('获取用户信息成功！');
+      renderAvatar(res.data);
+    },
+  });
+}
+// 渲染用户信息
+const renderAvatar = (user) => {
+  const name = user.nickname || user.username;
+  //渲染欢迎语
+  $('#welcome').html(`欢迎 ${name}`);
+  //按需求渲染头像  有上传头像用上传头像，没有就用文字头像
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user.user_pic).show();
+    $('.text-avatar').hide();
+  } else {
+    // 渲染文本头像
+    $('.layui-nav-img').hide();
+    let firstName = name[0].toUpperCase();
+    $('.text-avatar').html(firstName);
+  }
+};
+//退出登入
+$('#btnLogout').click(() => {
+  layer.confirm('是否退出登录?', { icon: 3, title: '提示' }, function (index) {
+    // 1、清除本地储存的token
+    localStorage.removeItem('token');
+    // 2、跳转至登录页面
+    location.href = '/login.html'
+  });
+});
+//获取用户列表
+getUserInfo();
